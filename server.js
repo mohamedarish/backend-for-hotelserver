f908e31dccac7cffcbbfba97b1faf89cdc77794b
Mohamed Arish <Arish1248@gmail.com>
2022-12-26 01:48:35 +0530

    Made a backend

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const app = express();
 const userRoutes = require("./routes/user");
 const cors = require("cors");
 
+app.use(http);
+
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
 
-app.options("/url...", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
-    res.header("Access-Control-Allow-Headers", "accept, content-type");
-    res.header("Access-Control-Max-Age", "1728000");
-    return res.sendStatus(200);
-});
+app.use(cors());
 
 app.listen(8000, () => {
     console.log("Connected to port 8000");
