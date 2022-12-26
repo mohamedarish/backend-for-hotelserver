53ca1d230aa8cf8aad97fe8bbfac5afc7988d26b
Mohamed Arish <Arish1248@gmail.com>
2022-12-26 10:17:44 +0530

    Cors not working?

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const userRoutes = require("./routes/user");
 const cors = require("cors");
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
-
-app.use(cors());
 
 app.listen(8000, () => {
     console.log("Connected to port 8000");
