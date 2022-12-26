aef875830a774eaf63b30f4140545ac0fa878015
Mohamed Arish <Arish1248@gmail.com>
2022-12-26 10:38:58 +0530

    Cors not working?

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user");
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(cors());
-// app.options("*", cors());
-
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json());
 
